3a2f37db60c56c68e55161fe50d836c7693d9ea7
Hensley, Chad <chad.hensley@verizonwireless.com>
2018-11-06 15:43:02 -0600

    update gminer to read config file for date range

--- gminer.py
+++ gminer.py
@@ -19,14 +19,12 @@ def mineRepo( repoPath , branch, startDate, stopDate, metaHandler):
     fileChangeCount = 0
 
     metaHandler.write('Branch_name, developer, changed_file, change_type, commit_date, commit_hash \n')
-    for commit in RepositoryMining(
-        repoPath, only_in_branch=branch,since=startDate, to=stopDate).traverse_commits():
+    for commit in RepositoryMining( repoPath, only_in_branch=branch,since=startDate, to=stopDate).traverse_commits():
           for modification in commit.modifications:
-              print('Branch {},Author {},modified {},change type {}, date {},commit {}'.format(branch, commit.author.name, modification.filename, modification.change_type, commit.committer_date,commit.hash))
               metaHandler.write('{},{},{},{},{},{} \n'.format(branch, commit.author.name, modification.filename, modification.change_type, commit.committer_date,commit.hash))
               fileChangeCount += 1
           commitCount += 1
-
+    print (" Branch {}, File Change {}, Commit Count {}".format(branch, fileChangeCount, commitCount))
     return commitCount, fileChangeCount
 
 if __name__ == "__main__":
@@ -35,14 +33,16 @@ if __name__ == "__main__":
     fileChangeCount = 0
     finalFileChangeCount = 0
 
-    to_zone = timezone(timedelta(hours=5))
-    startDate = datetime(2003, 5, 1, 0, 0, 0, tzinfo=to_zone)
-    stopDate = datetime(2017, 11, 1, 0, 0, 0, tzinfo=to_zone)
-
     config = getconfig ( './config.yaml' )
+
+    to_zone = timezone(timedelta(hours=config["TIME_ZONE_OFFSET"]))
+    startDate = datetime(config["START_YEAR"], config["START_MONTH"], config["START_DAY"], 0, 0, 0, tzinfo=to_zone)
+    stopDate = datetime(config["STOP_YEAR"], config["STOP_MONTH"], config["STOP_DAY"], 0, 0, 0, tzinfo=to_zone)
+
 
     metaHandler = openFile(config["META_FILE"], "w")
     countHandler = openFile(config["COUNT_FILE"], "w")
+
     for branch in config["BRANCHES"]:
       commitCount, fileChangeCount = mineRepo(config["LOCAL_REPO"], branch, startDate, stopDate, metaHandler)
       finalCommitCount += commitCount
@@ -51,5 +51,5 @@ if __name__ == "__main__":
 
     print ("Final Count ", finalCommitCount, finalFileChangeCount)
     countHandler.write ('finalCommitCount, finalFileChangeCount\n')
-    countHandler.write ('{},{}'.format(finalCommitCount ,finalFileChangeCount))
+    countHandler.write ('{},{}'.format(finalCommitCount,finalFileChangeCount))
     closeFile(countHandler)

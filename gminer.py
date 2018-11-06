834853d13232988d6e1a5d75efe9558afeec3224
Hensley, Chad <chad.hensley@verizonwireless.com>
2018-11-06 09:04:43 -0600

    added configuration file, keep counts and write to file

--- gminer.py
+++ gminer.py
@@ -1,27 +1,55 @@
 from pydriller import RepositoryMining, GitRepository
 from datetime import datetime, timezone, timedelta
+import yaml
+
+def getconfig ( configFile ):
+    configHandler = openFile(configFile, "r")
+    config=yaml.load(configHandler)
+    return config
+
+def openFile ( fileName, action ):
+    return open(fileName, action)
+
+def closeFile ( fileHandler ):
+    fileHandler.close()
 
 
-def mineRepo( repoPath , branch):
-    count = 0
-    to_zone = timezone(timedelta(hours=5))
-    dt1 = datetime(2003, 5, 1, 0, 0, 0, tzinfo=to_zone)
-    dt2 = datetime(2017, 11, 1, 0, 0, 0, tzinfo=to_zone)
+def mineRepo( repoPath , branch, startDate, stopDate, metaHandler):
+    commitCount = 0
+    fileChangeCount = 0
 
+    metaHandler.write('Branch_name, developer, changed_file, change_type, commit_date, commit_hash \n')
     for commit in RepositoryMining(
-        repoPath, only_in_branch=branch,since=dt1, to=dt2).traverse_commits():
-        count = count +1;
-        for modification in commit.modifications:
-            print('Branch {}, Author {}, modified {},date {}.commit {}'.format(branch, commit.author.name, modification.filename, commit.committer_date,commit.hash))
-        print('Final Count ', count);
+        repoPath, only_in_branch=branch,since=startDate, to=stopDate).traverse_commits():
+          for modification in commit.modifications:
+              print('Branch {},Author {},modified {},change type {}, date {},commit {}'.format(branch, commit.author.name, modification.filename, modification.change_type, commit.committer_date,commit.hash))
+              metaHandler.write('{},{},{},{},{},{} \n'.format(branch, commit.author.name, modification.filename, modification.change_type, commit.committer_date,commit.hash))
+              fileChangeCount += 1
+          commitCount += 1
+
+    return commitCount, fileChangeCount
 
 if __name__ == "__main__":
-    repoPath = ('/Users/chensle/emse/SE6356.MS1-SoftwareMaintenanceEvolutionAndRe-Engineering-F18/Projects/local_repo/peep-commons-math' )
-    branches = ["3.6-release", "3.6.1-release", "EXPERIMENTAL", "MANTISSA", "MATH-1153", "MATH_1_1",
-               "MATH_1_1_RC1_tmp", "MATH_1_3", "MATH_2_0_tmp", "MATH_2_X", "MATH_3_X", "complex-and-primitive-arrays",
-               "develop", "feature-MATH-1290", "feature-MATH-1333", "feature-MATH-1370", "feature-MATH-1372", "feature-MATH-1416",
-               "field-ode", "fix-math-1342", "h10-builds", "master", "master-old", "task-MATH-1366"]
-    gr = GitRepository(repoPath);
+    commitCount = 0
+    finalCommitCount = 0
+    fileChangeCount = 0
+    finalFileChangeCount = 0
 
-    for branch in branches:
-      mineRepo(repoPath, branch)
+    to_zone = timezone(timedelta(hours=5))
+    startDate = datetime(2003, 5, 1, 0, 0, 0, tzinfo=to_zone)
+    stopDate = datetime(2017, 11, 1, 0, 0, 0, tzinfo=to_zone)
+
+    config = getconfig ( './config.yaml' )
+
+    metaHandler = openFile(config["META_FILE"], "w")
+    countHandler = openFile(config["COUNT_FILE"], "w")
+    for branch in config["BRANCHES"]:
+      commitCount, fileChangeCount = mineRepo(config["LOCAL_REPO"], branch, startDate, stopDate, metaHandler)
+      finalCommitCount += commitCount
+      finalFileChangeCount += fileChangeCount
+    closeFile(metaHandler)
+
+    print ("Final Count ", finalCommitCount, finalFileChangeCount)
+    countHandler.write ('finalCommitCount, finalFileChangeCount\n')
+    countHandler.write ('{},{}'.format(finalCommitCount ,finalFileChangeCount))
+    closeFile(countHandler)

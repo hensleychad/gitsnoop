e3f812cc61c2e7bdca741e8f7dd120990ec03d8c
Hensley, Chad <chad.hensley@verizonwireless.com>
2018-11-16 22:48:58 -0600

    finished matrix

--- jiraNetworkContributors.py
+++ jiraNetworkContributors.py
@@ -1,5 +1,6 @@
 from sys import argv
 import pandas as pd
+import re
 
 
 def removeDuplicates(duplicate):
@@ -26,35 +27,36 @@ def buildUniqueUserList( ntwkDict ):
 def initMatrix( ntwkDict ):
   userDict = {}
   uniqueUsersLists = []
-  #print (uniqueUsersList)
   for uniqueUser_x in uniqueUsersList:
     userDict[uniqueUser_x] = {uniqueUser_x: 0}
-    count = 0
     for uniqueUser_y in uniqueUsersList:
       userDict[uniqueUser_x][uniqueUser_y] = 0
-      if (count == 40):
-        break
-      count = count +1
   return userDict
 
 def printResults (userDict):
   final_row = ""
-  count = 0
-  for user_x in userDict:
-    if (count == 40):
-      break
+
+  print("User,",sorted(userDict))
+  for user_x in sorted(userDict):
     for user_y in sorted(userDict[user_x]) :
-        final_row = final_row + "," + str(userDict[user_x][user_y])
-    count = count + 1
-    print (user_x, final_row)
+        if (userDict[user_x][user_y] == 0):
+          final_row = final_row + "," + "-"
+        else:
+           final_row = final_row + "," + str(userDict[user_x][user_y])
+    print (user_x.rstrip(),final_row)
     final_row = ""
 
-def buildNetwork (userDict):
+def buildNetwork (uniqueUsersList ,userDict):
     # test keys exist
-    user_xy = 'Andreas Lehmkuhler'
-    user_yx = 'ASF'
-    userDict[user_xy][user_yx] = userDict[user_xy][user_yx]  + 1
-    userDict[user_yx][user_xy] = userDict[user_yx][user_xy]  + 1
+    for uniqueUser_x in uniqueUsersList:
+      for uniqueUser_y in uniqueUsersList:
+        regex = r"(" + uniqueUser_y + ")"
+        if re.search(regex, uniqueUser_x):
+          count = 0 #this does nothing, do not feel like changing logic
+        else:
+          userDict[uniqueUser_x][uniqueUser_y] = userDict[uniqueUser_x][uniqueUser_y]  + 1
+          userDict[uniqueUser_y][uniqueUser_x] = userDict[uniqueUser_y][uniqueUser_x]  + 1
+
     return userDict
 
 
@@ -64,14 +66,16 @@ if __name__ == "__main__":
   transactionFile = argv[1]
   df = pd.read_csv(transactionFile)
   ntwkDict = df.to_dict()
+  userNetworkDict = {}
 
   uniqueUsersList = buildUniqueUserList(ntwkDict)
   userDict = initMatrix(uniqueUsersList)
-  userNetworkDict = buildNetwork(userDict)
-
+  for i in ntwkDict['network']:
+    userNetwork  = ntwkDict['network'][i]
+    if (userNetwork):
+      userNetworkList = userNetwork.split(',')
+      uniqueUsersLists = removeDuplicates(userNetworkList)
+      if (len(uniqueUsersLists) > 1):
+        userNetworkDict = buildNetwork(uniqueUsersLists,userDict)
   printResults(userNetworkDict)
-
-
-
-
 
